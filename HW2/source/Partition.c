b21cf8bc82573e46dfabf91b96ec125da27e4da4
Calvin Hsu <calvin820119@gmail.com>
2017-05-05 23:01:54 +0800

    modify partition.c & single_thread.c

--- HW2/source/Partition.c
+++ HW2/source/Partition.c
@@ -13,7 +13,7 @@
 #define size 20
 
 void MultiplyMatrix();
-void printSchedulerName(int PID);
+void printSchedulerName(int PID, int thread);
 
 int MatrixA[size][size]={0};
 int MatrixB[size][size]={0};
@@ -21,12 +21,21 @@ int MatrixC[size][size]={0};
  
 pid_t getpid();
 
+const char sched_mode_str[3][18] = {
+	"Normal",
+	"FIFO",
+	"Round-Robin"
+};
+typedef enum sched_mode_e{
+	Normal=0, 
+	FIFO, 
+	RR
+}sched_mode_t;
 
 int main(int argc, char** argv)
 {
 	srand(time(NULL));
-	int i,j,h, n=100000;
-	int mask;
+	int i,j,h, n=100000, mask;
 	float a[100000], b[100000], sum;
 	time_t second, second2;
 	int set_num_threads = 6;
@@ -34,11 +43,32 @@ int main(int argc, char** argv)
 	int ret,PID, cpu, prio;
 	int cpu_alloc[set_num_threads];
 	int migration_counter=0;
+	int sched_mode=0;	
 	struct sched_param sp;
-		
-	sp.sched_priority = sched_get_priority_max(SCHED_FIFO);
 
-	
+	if(2 != argc){
+		printf("wrong option input!\n\n");
+		exit(1);
+	}else{
+		sched_mode = *argv[1] - '0';
+		if(sched_mode > RR || sched_mode < Normal ){
+			printf("wrong option input!\n\n");
+			exit(1);
+		}
+		printf("Policy=%s\n\n", sched_mode_str[sched_mode]);
+	}
+
+	switch(sched_mode){
+		case Normal:
+			sp.sched_priority = sched_get_priority_max(SCHED_OTHER);			
+		case FIFO:
+			sp.sched_priority = sched_get_priority_max(SCHED_FIFO);
+		case RR:
+			sp.sched_priority = sched_get_priority_max(SCHED_RR);
+		default:
+			;
+	}
+
 	omp_set_num_threads(set_num_threads);
 
 	#pragma omp parallel num_threads(set_num_threads) private(PID, ret, set, mask)	
@@ -48,37 +78,32 @@ int main(int argc, char** argv)
 		CPU_SET((omp_get_thread_num()%(set_num_threads/2))+1, &set);
 		
 		ret=sched_setaffinity(PID, sizeof(cpu_set_t), &set);
-		sched_setscheduler(PID, SCHED_FIFO, &sp);
+		switch(sched_mode){
+			case Normal:
+				sched_setscheduler(PID, SCHED_OTHER, &sp);		
+			case FIFO:
+				sched_setscheduler(PID, SCHED_FIFO, &sp);	
+			case RR:
+				sched_setscheduler(PID, SCHED_RR, &sp);	
+			default:
+				;
+		}
 		cpu_alloc[omp_get_thread_num()] = sched_getcpu();
 		sched_getaffinity(PID, sizeof(mask), (cpu_set_t *)&mask);
-		printf("thread%d cpumask%d\n", (int)omp_get_thread_num(), mask);
+		printf("thread%d(%d) cpumask(0x%X)\n", (int)omp_get_thread_num(), PID, mask);
 	}
 
-	second2 = time(NULL);	
+	printf("\n");
 
-	/* Some initializations */
+	#pragma omp parallel num_threads(set_num_threads) private(PID)
+	{
+		printSchedulerName(getpid(), omp_get_thread_num());
+	}
 
 	for (i=0; i < n; i++) {
 		a[i] = i*1.0;
 		b[n-1-i] = i*1.0;
 	}
-	
-	sum = 0.0;
-	for (i=0; i < n; i++)
-	{
-		sum = sum + (a[i] * b[i]);
-		if(sched_getcpu()!=cpu_alloc[omp_get_thread_num()])
-		{
-			printf("[%d] The thread %d is moved from CPU%d to CPU%d \n", i, omp_get_thread_num(), cpu_alloc[omp_get_thread_num()], sched_getcpu());
-			cpu_alloc[omp_get_thread_num()]= sched_getcpu();
-			migration_counter++;
-		}
-	}
-	
-	//printf("\n Sum = %f\n",sum);
-
-	second = time(NULL);	
-	//printf("\n total seconds : %ld\n",second-second2);
 	
 
 	for(j=0;j<size;j++)
@@ -87,35 +112,11 @@ int main(int argc, char** argv)
 		{
 			MatrixA[i][j] = i*j;
 			MatrixB[i][j] = i+j;
-			
-			if(sched_getcpu()!=cpu_alloc[omp_get_thread_num()])
-			{
-				printf("The thread %d is moved from CPU%d to CPU%d \n", omp_get_thread_num(), cpu_alloc[omp_get_thread_num()], sched_getcpu());
-				cpu_alloc[omp_get_thread_num()]= sched_getcpu();
-				migration_counter++;	
-			}
 		}
 	}
-/*
-	printf("******************************************************\n");
-	printf("Matrix A:\n");
-	for (i=0; i<size; i++)
-	{
-	  for (j=0; j<size; j++) [i][j]);
-	  printf("\n"); 
-	 }
-	printf("******************************************************\n");
-	printf("Matrix B:\n");
-	for (i=0; i<size; i++)
-	{
-	  for (j=0; j<size; j++) 
-	    printf("%d \t", MatrixB[i][j]);
-	  printf("\n"); 
-	 }
-	printf("******************************************************\n");
-*/
+
+	printf("\n");
 	second2 = time(NULL);
-	
 	migration_counter = 0;
 
 	#pragma omp parallel for num_threads(set_num_threads) private(j)
@@ -133,19 +134,6 @@ int main(int argc, char** argv)
 		}
 	}
 	
-	/*** Print results ***/
-/*
-	printf("******************************************************\n");
-	printf("Result Matrix:\n");
-	for (i=0; i<size; i++)
-	{
-	  for (j=0; j<size; j++) 
-	    printf("%d \t", MatrixC[i][j]);
-	  printf("\n"); 
-	 }
-	printf("******************************************************\n");
-	printf ("Done.\n");
-*/
 	printf("Total number of migration is %d \n", migration_counter);
 
 	second = time(NULL);	
@@ -166,29 +154,28 @@ void MultiplyMatrix()
 			for(k=0;k<size;k++)
 			{
 				MatrixC[i][j] = MatrixC[i][j]+MatrixB[i][j]* MatrixA[k][j];
-				//printf("%d  %d   %d ", MatrixC[i][j],MatrixB[i][j],MatrixA[k][j]);
 			}		
 		}
 	}
 }
 
-void printSchedulerName(int PID){	
-	int policy = sched_getscheduler(PID);	
+void printSchedulerName(int PID, int thread){	
+	int policy = sched_getscheduler(PID+thread);	
 	switch(policy){		
 	  case SCHED_OTHER:
-		printf("Thread: %d, Policy: Normal\n", PID);
+		printf("Thread%d(%d) Policy: Normal\n", thread, PID+thread);
 		break;
 	  case SCHED_RR:
-		printf("Thread: %d, Policy: Round-Robin\n", PID);
+		printf("Thread%d(%d) Policy: Round-Robin\n", thread, PID+thread);
 		break;
 	  case SCHED_FIFO:
-		printf("Thread: %d, Policy: FIFO\n", PID);
+		printf("Thread%d(%d) Policy: FIFO\n", thread, PID+thread);
 		break;	
 	  case -1:
 		perror("sched_getscheduler");
 		break;
 	  default:
-		printf("Thread: %d, Policy: Unknown\n", PID);
+		printf("Thread%d(%d) Policy: Unknown\n", thread, PID+thread);
 		break;	
 	}
 }
--- HW2/source/Single_thread.c
+++ HW2/source/Single_thread.c
@@ -13,7 +13,7 @@
 #define size 20
 
 void MultiplyMatrix();
-void printSchedulerName(int PID);
+void printSchedulerName(int PID, int thread);
 
 int MatrixA[size][size]={0};
 int MatrixB[size][size]={0};
@@ -21,12 +21,22 @@ int MatrixC[size][size]={0};
  
 pid_t getpid();
 
+const char sched_mode_str[3][18] = {
+	"Normal",
+	"FIFO",
+	"Round-Robin"
+};
+typedef enum sched_mode_e{
+	Normal=0, 
+	FIFO, 
+	RR
+}sched_mode_t;
 
 int main(int argc, char** argv)
 {
 	int set_num_threads = 1;	
 	srand(time(NULL));
-	int i,j,h, n=100000;
+	int i,j,h, n=100000, mask;
 	float a[100000], b[100000], sum;
 	time_t second, second2;
 	
@@ -34,39 +44,60 @@ int main(int argc, char** argv)
 	int ret,PID, cpu, prio;
 	int cpu_alloc[set_num_threads];
 	int migration_counter=0;
+	int sched_mode=0;
+	struct sched_param sp;
+
+	if(2 != argc){
+		printf("wrong option input!\n\n");
+		exit(1);
+	}else{
+		sched_mode = *argv[1] - '0';
+		if(sched_mode > RR || sched_mode < Normal ){
+			printf("wrong option input!\n\n");
+			exit(1);
+		}
+		printf("Policy=%s\n\n", sched_mode_str[sched_mode]);
+	}
 	
+	switch(sched_mode){
+		case Normal:
+			sp.sched_priority = sched_get_priority_max(SCHED_OTHER);			
+		case FIFO:
+			sp.sched_priority = sched_get_priority_max(SCHED_FIFO);
+		case RR:
+			sp.sched_priority = sched_get_priority_max(SCHED_RR);
+		default:
+			;
+	}
+
 	omp_set_num_threads(set_num_threads);
 
 	CPU_ZERO(&set);
 	CPU_SET(1, &set);
+	CPU_SET(2, &set);
+	CPU_SET(3, &set);
 	ret=sched_setaffinity(0, sizeof(cpu_set_t), &set);	
+	switch(sched_mode){
+		case Normal:
+			sched_setscheduler(0, SCHED_OTHER, &sp);		
+		case FIFO:
+			sched_setscheduler(0, SCHED_FIFO, &sp);	
+		case RR:
+			sched_setscheduler(0, SCHED_RR, &sp);	
+		default:
+			;
+	}
+	PID=getpid()+omp_get_thread_num();
+	cpu_alloc[omp_get_thread_num()] = sched_getcpu();
+	sched_getaffinity(PID, sizeof(mask), (cpu_set_t *)&mask);
+	printf("thread%d(%d) cpumask(0x%X)\n\n", (int)omp_get_thread_num(), PID, mask);
 	
-	
-	second2 = time(NULL);	
-
-	/* Some initializations */
+	printSchedulerName(getpid(), omp_get_thread_num());
 
 	for (i=0; i < n; i++) {
 		a[i] = i*1.0;
 		b[n-1-i] = i*1.0;
 	}
-	sum = 0.0;
-
-	for (i=0; i < n; i++)
-	{
-		sum = sum + (a[i] * b[i]);
-		if(sched_getcpu()!=cpu_alloc[omp_get_thread_num()])
-		{
-			printf("[%d] The thread %d is moved from CPU%d to CPU%d \n", i, omp_get_thread_num(), cpu_alloc[omp_get_thread_num()], sched_getcpu());
-			cpu_alloc[omp_get_thread_num()]= sched_getcpu();
-			migration_counter++;
-		}
-	}
-	
-	//printf("\n Sum = %f\n",sum);
-
-	second = time(NULL);	
-	//printf("\n total seconds : %ld\n",second-second2);
 	
 
 	for(j=0;j<size;j++)
@@ -75,43 +106,17 @@ int main(int argc, char** argv)
 		{
 			MatrixA[i][j] = i*j;
 			MatrixB[i][j] = i+j;
-			
-			if(sched_getcpu()!=cpu_alloc[omp_get_thread_num()])
-			{
-				printf("The thread %d is moved from CPU%d to CPU%d \n", omp_get_thread_num(), cpu_alloc[omp_get_thread_num()], sched_getcpu());
-				cpu_alloc[omp_get_thread_num()]= sched_getcpu();
-				migration_counter++;	
-			}
 		}
 	}
-/*
-	printf("******************************************************\n");
-	printf("Matrix A:\n");
-	for (i=0; i<size; i++)
-	{
-	  for (j=0; j<size; j++)
- 	  printf("%d \t", MatrixA[i][j]);
-	  printf("\n"); 
-	 }
-	printf("******************************************************\n");
-	printf("Matrix B:\n");
-	for (i=0; i<size; i++)
-	{
-	  for (j=0; j<size; j++) 
-	    printf("%d \t", MatrixB[i][j]);
-	  printf("\n"); 
-	 }
-	printf("******************************************************\n");
-*/
+
+	printf("\n");
 	second2 = time(NULL);
-	
 	migration_counter = 0;
 
 	for (i=0; i < 3000; i++)
 	{
 		for (j=0; j<3000; j++) 
 		{
-			//printf("Thread: %d i:%d j:%d\n", omp_get_thread_num(), i, j);
 			MultiplyMatrix();
 			if(sched_getcpu()!=cpu_alloc[omp_get_thread_num()])
 			{
@@ -122,19 +127,6 @@ int main(int argc, char** argv)
 		}
 	}
 	
-	/*** Print results ***/
-/*
-	printf("******************************************************\n");
-	printf("Result Matrix:\n");
-	for (i=0; i<size; i++)
-	{
-	  for (j=0; j<size; j++) 
-	    printf("%d \t", MatrixC[i][j]);
-	  printf("\n"); 
-	 }
-	printf("******************************************************\n");
-	printf ("Done.\n");
-*/
 	printf("Total number of migration is %d \n", migration_counter);
 
 	second = time(NULL);	
@@ -155,29 +147,28 @@ void MultiplyMatrix()
 			for(k=0;k<size;k++)
 			{
 				MatrixC[i][j] = MatrixC[i][j]+MatrixB[i][j]* MatrixA[k][j];
-				//printf("%d  %d   %d ", MatrixC[i][j],MatrixB[i][j],MatrixA[k][j]);
 			}		
 		}
 	}
 }
 
-void printSchedulerName(int PID){	
-	int policy = sched_getscheduler(PID);	
+void printSchedulerName(int PID, int thread){	
+	int policy = sched_getscheduler(PID+thread);	
 	switch(policy){		
 	  case SCHED_OTHER:
-		printf("Thread: %d, Policy: Normal\n", PID);
+		printf("Thread%d(%d) Policy: Normal\n", thread, PID+thread);
 		break;
 	  case SCHED_RR:
-		printf("Thread: %d, Policy: Round-Robin\n", PID);
+		printf("Thread%d(%d) Policy: Round-Robin\n", thread, PID+thread);
 		break;
 	  case SCHED_FIFO:
-		printf("Thread: %d, Policy: FIFO\n", PID);
+		printf("Thread%d(%d) Policy: FIFO\n", thread, PID+thread);
 		break;	
 	  case -1:
 		perror("sched_getscheduler");
 		break;
 	  default:
-		printf("Thread: %d, Policy: Unknown\n", PID);
+		printf("Thread%d(%d) Policy: Unknown\n", thread, PID+thread);
 		break;	
 	}
 }
